97870d9ac8968b656a7f98802575f43c34d1e30f
Asmaa Sherif ElKholy <aselkholy461997@Gmail.com>
2020-06-23 23:13:02 +0200

    Added getVideoDetails Request

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,18 +1,24 @@
+import { Mixins } from 'vue-property-decorator';
+
 import axios from 'axios';
 
 import store from '@/store';
+import DateTimeMixin from '@/mixins/DateTimeMixin';
 
 // https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=over and over again&key=
 
-export default class AxiosClient {
+export default class AxiosClient extends Mixins(DateTimeMixin) {
   private static axiosClient: AxiosClient;
 
   private baseApiURL = 'https://www.googleapis.com/youtube/v3/';
-  private apiKey = 'AIzaSyA6y_OuMGhA9fGJIpirKMJx3IclpMogiU0';
+  // private apiKey = 'AIzaSyA6y_OuMGhA9fGJIpirKMJx3IclpMogiU0';
+  private apiKey = 'AIzaSyAFaGDINuSRAK8Pk0NgPGk8fmh3hav73PM';
 
-  private maxResults = 25;
+  private maxResults = 2;
 
-  private constructor() {}
+  private constructor() {
+    super();
+  }
 
   public static getInstance(): AxiosClient {
     if (!this.axiosClient) this.axiosClient = new AxiosClient();
@@ -27,27 +33,15 @@ export default class AxiosClient {
       url += filters.order ? `&order=${filters.order}` : '';
       url += filters.uploadDate ? `&publishedAfter=${this.calculateUploadDate(filters.uploadDate)}` : '';
     }
-    return axios.get(url).then((response) => {
+    return axios.get(url).then(response => {
       store.dispatch('populateSearchResult', response.data);
       store.commit('toggleIsLoading');
     });
   }
 
-  private calculateUploadDate(uploadDate: string) {
-    const youtubeDateFormat = new Date(Date.now());
-    if (uploadDate === 'Last hour') youtubeDateFormat.setMinutes(0, 0, 0);
-    else if (uploadDate === 'Today') youtubeDateFormat.setHours(0, 0, 0, 0);
-    else if (uploadDate === 'This week') {
-      const firstDay = youtubeDateFormat.getDate() - youtubeDateFormat.getDay();
-      youtubeDateFormat.setDate(firstDay);
-      youtubeDateFormat.setHours(0, 0, 0, 0);
-    } else if (uploadDate === 'This month') {
-      youtubeDateFormat.setMonth(youtubeDateFormat.getMonth(), 1);
-      youtubeDateFormat.setHours(0, 0, 0, 0);
-    } else {
-      youtubeDateFormat.setMonth(0, 1);
-      youtubeDateFormat.setHours(0, 0, 0, 0);
-    }
-    return youtubeDateFormat.toISOString();
+  public async getVideoDetails(videoId: string) {
+    const url = `${this.baseApiURL}videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${this.apiKey}`;
+    const response = await axios.get(url);
+    return response.data;
   }
 }

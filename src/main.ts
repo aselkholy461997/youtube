38a13fe9ce859af57af518a7c8d71f446f5276b2
Asmaa Sherif ElKholy <aselkholy461997@Gmail.com>
2020-06-23 12:07:08 +0200

    Formatted code

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 // import '@/assets/styles/app.scss'
 
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');

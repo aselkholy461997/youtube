1035a30464aade396e63152ec6052dd0389efdfd
Asmaa Sherif ElKholy <aselkholy461997@Gmail.com>
2020-06-23 23:07:52 +0200

    Added Models

---
+++ src/models/VideoDetails.ts
@@ -0,0 +1,46 @@
+import YoutubeItem from './YoutubeItem';
+import Thumbnails from './Thumbnails';
+
+export default class VideoDetails extends YoutubeItem {
+  items!: {
+    kind: string;
+    etag: string;
+    id: string;
+    snippet?: {
+      publishedAt: string;
+      channelId: string;
+      title: string;
+      description: string;
+      thumbnails: Thumbnails;
+      channelTitle: string;
+      tags: string[];
+      categoryId: string;
+      liveBroadcastContent: string;
+      localized: {
+        title: string;
+        description: string;
+      };
+      defaultAudioLanguage: string;
+    };
+    contentDetails?: {
+      duration: string;
+      dimension: string;
+      definition: string;
+      caption: string;
+      licensedContent: boolean;
+      contentRating: any;
+      projection: string;
+    };
+    statistics?: {
+      viewCount: string;
+      likeCount: string;
+      dislikeCount: string;
+      favoriteCount: string;
+      commentCount: string;
+    };
+  }[];
+  pageInfo!: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+}

1cb60b3e90abc29b24051d46053627a655739737
Asmaa Sherif ElKholy <aselkholy461997@Gmail.com>
2020-06-25 00:06:40 +0200

    Added PlaylistDetails Model

---
+++ src/models/PlaylistDetails.ts
@@ -0,0 +1,30 @@
+import YoutubeItem from './YoutubeItem';
+import Thumbnails from './Thumbnails';
+
+export default class VideoDetails extends YoutubeItem {
+  items!: {
+    kind: string;
+    etag: string;
+    id: string;
+    snippet?: {
+      publishedAt: string;
+      channelId: string;
+      title: string;
+      description: string;
+      thumbnails: Thumbnails;
+      channelTitle: string;
+      playlistId: string;
+      liveBroadcastContent: string;
+      publishTime: string;
+      position: number;
+      resourceId: {
+        kind: string; 
+        videoId: string;
+      };
+    };
+  }[];
+  pageInfo!: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+}

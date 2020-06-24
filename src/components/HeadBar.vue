40c8bc434f90576e4f213e666fcacc469832ae2a
Asmaa Sherif ElKholy <aselkholy461997@Gmail.com>
2020-06-25 01:00:49 +0200

    Update HeadBar.vue

--- src/components/HeadBar.vue
+++ src/components/HeadBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="desktop-header">
-    <img class="logo" src="../assets/desktop-logo.png" />
+  <div id="app-header">
+    <img class="logo cursor-pointer" />
     <div class="search-container">
       <input
         type="search"
@@ -17,6 +17,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import store from '@/store';
 
 import AxiosClient from '../api/index';
 
@@ -25,21 +26,33 @@ export default class HeadBar extends Vue {
   axiosClient = AxiosClient.getInstance();
   searchQuery = '';
 
+  mounted() {
+    if (!store.state.isLoading && !store.state.searchResult.items) {
+      const searchQuery = this.$route.query.query as string;
+
+      if (searchQuery && searchQuery.trim()) {
+        this.searchQuery = searchQuery.trim();
+        this.axiosClient.getSearchResults(this.searchQuery.trim());
+      }
+    }
+  }
+
   goToSearch(): void {
     if (this.searchQuery.trim()) {
-      this.axiosClient.getSearchResults(this.searchQuery.trim());
-      if (this.$route.fullPath !== `/search?query=${this.searchQuery.trim()}`)
+      if (this.$route.fullPath !== `/search?query=${this.searchQuery.trim()}`) {
+        this.axiosClient.getSearchResults(this.searchQuery.trim());
         this.$router.push({
           path: '/search',
           query: { query: this.searchQuery.trim() }
         });
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.desktop-header {
+#app-header {
   height: 2.5rem;
   display: flex;
   flex-direction: row;
@@ -47,56 +60,115 @@ export default class HeadBar extends Vue {
   flex-basis: content;
   align-items: center;
   justify-content: space-between;
-  padding: 12px 15%;
-  // border-bottom: 1px solid #dbdbdb;
+  padding: 12px;
+
   box-shadow: 0 1px 10px -5px #888888;
   position: relative;
+  background-color: #ff0000;
+
+  @media (min-width: 600px) {
+    background-color: #ffffff;
+    padding: 12px 15%;
+  }
 
   .search-button {
-    background-image: url('../assets/search-icon.jpg');
-    background-size: 15px;
+    background-image: url('../assets/mobile-search-icon.png');
+    background-size: 1rem;
     padding: 0px;
     margin: 0px;
     background-repeat: no-repeat;
     background-position: center;
-    width: 4.5rem;
-    border-radius: 0px 3px 3px 0px;
+    background-color: transparent;
+    width: 3rem;
+    border-width: 0;
+
+    @media (min-width: 600px) {
+      border-radius: 0px 3px 3px 0px;
+      background-image: url('../assets/desktop-search-icon.png');
+      background-color: #efefef;
+      width: 4.5rem;
+      border-width: thin;
+    }
   }
 
   .logo {
     height: auto;
     max-height: 60%;
-    max-width: 20%;
-  }
+    content: url('../assets/mobile-logo.png');
 
-  .search-field {
-    width: calc(100% - 4.5rem);
-    box-shadow: 0 0 2px #dbdbdb;
-    border-radius: 3px 0px 0px 3px;
-    padding-right: 0.5rem;
-    padding-left: 0.5rem;
+    @media (min-width: 600px) {
+      content: url('../assets/desktop-logo.png');
+      max-width: 20%;
+    }
   }
 
   .search-button,
   .search-field {
     height: 100%;
     outline: none;
-    border-color: groove #dbdbdb;
-    border-width: thin;
   }
+
+  .search-field {
+    width: 100%;
+    box-shadow: 0 0 2px #dbdbdb;
+    padding-right: 0.5rem;
+    padding-left: 0.5rem;
+    border-width: 0;
+    color: white;
+    background: transparent;
+    box-shadow: none;
+
+    @media (min-width: 600px) {
+      border-radius: 3px 0px 0px 3px;
+      border-width: thin;
+      color: black;
+      background: white;
+    }
+  }
+
+  // .search-button,
+  // .search-field {
+  //   height: 100%;
+  //   outline: none;
+  //   border: none;
+  //   color: white;
+  //   background: transparent;
+
+  //   @media (min-width: 600px) {
+  //     border: thick;
+  //     color: black;
+  //     background-color: white;
+  //   }
+  // }
 
   .search-field:focus,
   textarea:focus {
-    box-shadow: 0 0 2px #065fd4;
-    // padding: 3px 0px 3px 3px;
-    border-color: #065fd4;
+    color: black;
+    background: white;
+
+    @media (min-width: 600px) {
+      box-shadow: 0 0 2px #065fd4;
+      border-color: #065fd4;
+    }
   }
 
   .search-container {
     //margin-left: 50px;
     height: 100%;
     display: flex;
-    width: 60%;
+    width: 100%;
+    margin-left: 1rem;
+    @media (min-width: 600px) {
+      margin-left: 0;
+      width: 60%;
+    }
+
+    ::placeholder {
+      color: white;
+      @media (min-width: 600px) {
+        color: black;
+      }
+    }
   }
 }
 </style>
